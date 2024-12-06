c6b047b465341b8c1a38e6f6735369df62984879
Makayla J <makaylaj14@gmail.com>
2024-12-05 21:23:52 -0500

    completed my half of summary page

--- client/src/pages/summary.tsx
+++ client/src/pages/summary.tsx
@@ -1,9 +1,27 @@
-const Summary = () => {
-    return (
-      <section>
-        <h1>Summary</h1>
-      </section>
-    );
-  };
-  
-  export default Summary;
+const summary = () => {
+  return (
+    <section className="summary-container">
+      <h1>Almost Done! 🚀</h1>
+      <p>Confirm your deal details information and submit to create it.</p>
+      <br />
+      <div className="details-container">
+        <div className="service-types">
+          <h4>Service Type</h4>
+          <h4>Additional Service</h4>
+          <h4>Dwelling Type</h4>
+          <h4>Address</h4>
+          <h4>Square Footage</h4>
+        </div>
+        <div className="response">
+          <p>Walk & Talk</p>
+          <p>Blue Tape</p>
+          <p>Condo</p>
+          <p>101 Project2 Road, Washington DC, 10000</p>
+          <p>1000 sqft</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default summary;
