35a80a7510b41e6bfea2a1bd70cffdae2a73ad33
Marcel Peyton <marcel-p@outlook.com>
2024-12-08 16:26:03 -0500

    fixing authRoutes.ts

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 // /src/routes/authRouter.ts
 
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
 import { getAuthUrl, getTokens } from '../config/connection.js';
 
 const router = Router();
@@ -27,5 +27,35 @@ router.get('/callback', async (req, res) => {
     res.status(400).send('Authorization code missing');
   }
 });
+
+import { User } from '../models/user.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+export const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: { username },
+  });
+  if (!user) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const passwordIsValid = await bcrypt.compare(password, user.password);
+  if (!passwordIsValid) {
+    return res.status(401).json({ message: 'Authentication failed' });
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });
+};
+
+
+
+// POST /login - Login a user
+router.post('/login', login);
 
 export default router;
