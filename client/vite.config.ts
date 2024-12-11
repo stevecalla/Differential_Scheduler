0067370e2e7fd76152aa7d305756d8ff571e2648
Steve Calla <callasteven@gmail.com>
2024-12-10 23:12:51 -0700

    Adjust vite config to add react plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,4 +19,5 @@ export default defineConfig({
       },
     },
   },
+  plugins: [react()],
 });
