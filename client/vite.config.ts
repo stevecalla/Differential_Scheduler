1d2b815cc49803bd809b26c2d4d7caadce1ad4a4
Steve Calla <callasteven@gmail.com>
2024-12-10 22:56:14 -0700

    Remove chunk and rollp otions

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,33 +2,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    chunkSizeWarningLimit: 100,
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === "MODULE_LEVEL_DIRECTIVE") {
-          return;
-        }
-        warn(warning);
-      },
-        output: {
-          chunkFileNames: 'JavaScript/[name]-[hash].js',
-          entryFileNames: 'JavaScript/[name]-[hash].js',
-      
-          assetFileNames: ({name}) => {
-            if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
-              return 'images/[name]-[hash].[extname]';
-            }
-      
-            if (/\.css$/.test(name ?? '')) {
-              return 'CSS/[name]-[hash][extname]';
-            }
-      
-            return '[name]-[hash].[extname]';
-          },
-        },
-    },
-  },
   server: {
     port: 3000,
     open: true,
